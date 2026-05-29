96263e4a02bf9696ea44dbb0a967a5366b911b64
BlackLinux <nikonorovsasha1@gmail.com[C>
2026-05-29 16:43:12 +0300

    Привел репозиторий в порядок

---
+++ src/main.cpp
@@ -0,0 +1,87 @@
+#include "header.h"
+
+sf::VideoMode desktop = sf::VideoMode::getDesktopMode();
+    
+int width = desktop.width;
+int height = desktop.height;
+
+int sosx = 1500, sosy = sosx * height / width;
+
+
+
+int main()
+{
+    srand(time(0));
+    ContextSettings settings;
+    settings.antialiasingLevel = 5;
+    RenderWindow window(VideoMode(sosx, sosy), "Solar System", Style::Default, settings);
+
+    //Planet planet1(50, 5,Vector2f(900, 100));
+    //Planet planet2(500, 10, Vector2f(950, 200));
+    Universe universe(5);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        window.clear(sf::Color(255, 255, 255));
+
+        //planet1.acc_calc(planet2);
+        //planet2.acc_calc(planet1);
+
+
+        //planet1.phy();
+        //planet2.phy();
+        universe.physics();
+
+        universe.draw_planets(window);
+        //window.draw(planet1.get_body());
+        //window.draw(planet2.get_body());
+
+        window.display();
+    }
+
+    return 0;
+}
+
+float dis(Vector2f a, Vector2f b) {
+    return pow((a.x - b.x) * (a.x - b.x) + (a.y - b.y) * (a.y - b.y), 0.5);
+}
+
+float mod(Vector2f a) {
+    return pow(a.x * a.x + a.y * a.y, 0.5);
+}
+
+//float angle_between_vetors(Vector2f one, Vector2f two) {
+//    if (mod(one) == 0) return 0;
+//    return acosf(mod(two / mod(one)));
+//}
+
+Vector2f normalize(Vector2f orig) {
+    if (mod(orig) == 0) return Vector2f(0, 0);
+    return orig / mod(orig);
+}
+
+//Vector2f rotate_vec(Vector2f vec, float angle) {
+//    if (normalize(vec).x == 0) return vec;
+//    //if (cosf(acosf(1.0f / normalize(vec).x) + angle) || sinf(acosf(1.0f / normalize(vec).x) + angle)) return vec;
+//    return mod(vec) * Vector2f(cosf(acosf(normalize(vec).x) + angle), sinf(acosf(normalize(vec).x) + angle));
+//}
+//
+//Vector2f to_normal(Vector2f one, Vector2f nor) {
+//    return rotate_vec(one, angle_between_vetors(one, nor));
+//}
+
+//Vector2f to_global(Vector2f one, Vector2f nor) {
+//    return rotate_vec(one, -angle_between_vetors(one, nor));
+//}
+
+bool co_dir(float a, float b) {
+    if ((a >= 0) && (b >= 0) || (a <= 0) && (b <= 0)) return true;
+    return false;
+}
